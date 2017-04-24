51c3964b41505fddbebd79ea02c1b16c64c24a4a
HaiGe614 <wang.4912@osu.edu>
2017-04-24 14:06:35 -0400

    lab-nineex01

---
+++ Source/BuckeyeStore.Core/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BuckeyeStore.Core
+{
+    public class Product
+    {
+        public int ID {get; set;}
+        public string Name {get; set;}
+        public string Description {get; set;}
+        public string Brand {get; set;}
+        public Review Review {get; set;}
+        public AggregateRating AggregateRating {get; set;}
+        public List<Offer> Offers {get; set;}
+        public String Sku {get; set;}
+
+        public void AddReview(Review review)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/BuckeyeStore.Core/Review.cs
@@ -0,0 +1,9 @@
+namespace BuckeyeStore.Core
+{
+    public class Review
+    {
+        public int ID {get; set;}
+        public string Body {get; set;}
+        public Rating Rating {get; set;}
+    }
+}
